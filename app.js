e19071f2dc872cdfcaba6a6c1868093a9da37f28
shahzod <shoxzodbek15@gmail.com>
2024-09-21 22:24:45 +0500

    sdgd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Boolean-1
-// let a = prompt('son kiriting');
-// let natija = (a % 2 === 0) ? "A soni juft" : "A soni toq";
-// console.log(natija);
+let a = prompt('son kiriting');
+let natija = (a % 2 === 0) ? "A soni juft" : "A soni toq";
+console.log(natija);
 
 // Boolean-2
 // let a = prompt('a sonini kiritng')
@@ -123,3 +123,121 @@
 // Boolen-12 ( Ishlap biladim )
 
 // Boolen-13
+// function isIncreasingNumber(number) {
+//     let a = Math.floor(number / 100);
+//     let b = Math.floor((number % 100) / 10);
+//     let c = number % 10;
+
+
+//     if (a < b && b < c) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// let number = prompt('3Xonali son kiriting;');
+// if (isIncreasingNumber(number)) {
+//     console.log("Sonning raqamlari ketma-ket o'suvchi.");
+// } else {
+//     console.log("Sonning raqamlari ketma-ket o'suvchi emas.");
+// }
+
+
+// Boolean-15
+// function isPalindrome(number) {
+//     let a = Math.floor(number / 100);
+//     let b = Math.floor((number % 100) / 10);
+//     let c = number % 10;
+
+//     if (a === c) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// let number = prompt('3Xonali son kiriting;');
+
+// if (isPalindrome(number)) {
+//     console.log("Son chapdan va o'ngdan o'qilganda bir xil.");
+// } else {
+//     console.log("Son chapdan va o'ngdan o'qilganda bir xil emas.");
+// }
+
+// Darsda ko'riladigan masalalar
+// 1
+// let a = prompt('a ga qiymat bering:');
+// let b = prompt('b ga qiymat bering:');
+// let y = -(b/2) *(a**2 +b**2)
+// console.log(y);
+
+// 2
+// let a = prompt('a ga qiymat bering:');
+// let b = prompt('b ga qiymat bering:');
+// let c = prompt('c ga qiymat bering:');
+// let d = prompt('d ga qiymat bering:');
+// let y = ((a + b) / (c + d)) **2 + b  ** 2;
+// console.log(y);
+
+// 3
+// let a = prompt('a ga qiymat bering: ');
+// let b = prompt('b ga qiymat bering: ');
+// let d = prompt('a ga adaraja bering: ');
+// let c = prompt('c ga qiymat bering: ');
+// const y = 2 * (a ** d + (4 * c ** 2) / 3);
+// console.log(y);
+
+// 4
+// let  a = prompt('a ga qiymat bering: ');
+// let  b = prompt('b ga qiymat bering: ');
+// let  c = prompt('c ga qiymat bering: ');
+// let  d = prompt('d ga qiymat bering: ');
+// const y = 1/a**2 * (b/10)**3 *  (c+d)**2;
+// console.log(y);
+
+// 5 
+// let a = prompt('a ga qiymat bering: ');
+// let b = prompt('b ga qiymat bering: ');
+// let c = prompt('c ga qiymat bering: ');
+// let d = prompt('d ga qiymat bering: ');
+// const y = (a / b - 1 / c / d-1)**2;
+// console.log(y);
+
+// 6
+// let a = prompt('a ga qiymat bering: ');
+// let b = prompt('b ga qiymat bering: ');
+// let c = prompt('c ga qiymat bering: ');
+// let d = prompt('d ga qiymat bering: ');
+// const y = a + b / c + (d / a + c);
+// console.log(y);
+
+// 7
+// let a = prompt('a ga qiymat bering: ');
+// let b = prompt('b ga qiymat bering: ');
+// let c = prompt('c ga qiymat bering: ');
+// let d = prompt('d ga qiymat bering: ');
+// const y = Math.sqrt((a + b + c) ** 2 + (a - b - c) ** 2);
+// console.log(y);
+
+// 8 
+// let a = prompt('a ga qiymat bering: ');
+// let b = prompt('b ga qiymat bering: ');
+// let c = prompt('c ga qiymat bering: ');
+// let d = prompt('d ga qiymat bering: ');
+// const y = (a ** 2 + b ** 2 + c ** 2) / (a * b * c);
+// console.log(y);
+
+// 9 
+// let a = prompt('a ga qiymat bering: ');
+// let b = prompt('b ga qiymat bering: ');
+// let c = prompt('c ga qiymat bering: ');
+// let d = prompt('d ga qiymat bering: ');
+// const y = a * (d ** 3 / 3) + b * (c ** 2 / c);
+// console.log(y);
+
+// 10
+// let  a = prompt('a ga qiymat bering: ');
+// let  b = prompt('b ga qiymat bering: ');
+// let  c = prompt('c ga qiymat bering: ');
+// let  d = prompt('d ga qiymat bering: ');
