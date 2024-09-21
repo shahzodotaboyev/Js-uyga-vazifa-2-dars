567cdf4a3440cd92aed8670160b7f801b297939a
shahzod <shoxzodbek15@gmail.com>
2024-09-21 13:21:53 +0500

    fd

---
+++ app.js
@@ -0,0 +1,79 @@
+// Boolean-1
+// let a = prompt('son kiriting');
+// let natija = (a % 2 === 0) ? "A soni juft" : "A soni toq";
+// console.log(natija);
+
+// Boolean-2
+// let a = prompt('a sonini kiritng')
+// let b = prompt('b sonini kiritng')
+// if (a >= 0 || b < -2) {
+//     console.log("Jumla rost");
+// }else{
+//     console.log("jummla yonlgon")
+// }
+
+// Boolean-3
+// let a = prompt('a ga son kiriting:')
+// let b = prompt('b ga son kiriting:')
+// let c = prompt('c ga son kiriting:')
+// if ((a > b && c < b) || (a < b && c > b)) {
+//     console.log("b son a va c sonini o'rtasida");
+// } else {
+//     console.log("b son a va c sonini o'rtasida emas");
+
+// }
+
+// Boolean-4
+// let a = prompt('a ga son kiritng:')
+// let b = prompt('b ga son kiritng:')
+// if (a % 2 !== 0 && b % 2 !== 0) {
+//     console.log(" a va b son toq")
+// } else {
+//     console.log(" a yoki b soni juft ")
+// }
+
+// Boolean-5
+// let a = prompt('a ga son kiritng:')
+// let b = prompt('b ga son kiritng:')
+// if (a % 2 !== 0 || b % 2 !== 0) {
+//     console.log(' a yoki b toq ');
+// } else {
+//     console.log('ikkalasiham juft')
+// }
+
+// Boolean-6
+// let a = prompt('a ga son kiritng:')
+// let b = prompt('b ga son kiritng:')
+// if ((a % 2 !== 0 && b % 2 == 0) || (a % 2 == 0 && b % 2 !== 0)) {
+
+//     console.log(' a yoki b soni toq');
+// } else {
+//     console.log(' ikkalasiha toq yoki  juft')
+// }
+
+// Boolean-7
+// let a = prompt('a ga son kiritng:');
+// let b = prompt('b ga son kiritng:');
+// let c = prompt('c ga son kiritng:');
+// if (a > 0 ||  b < 0 || c > 0) { 
+//     console.log('a , b va c soni manfiy');
+
+// }else{
+//     console.log('a yoki b yoki c musbat')
+// }
+
+// Boolen-8
+
+// function isOnlyOnePositive(A, B, C) {
+//     const positiveCount = (A > 0 ? 1 : 0) + (B > 0 ? 1 : 0) + (C > 0 ? 1 : 0);
+//     return positiveCount === 1;
+// } 
+// let a = prompt('a ga son bering')
+// let b = prompt('b ga son bering')
+// let c = prompt('c ga son bering');
+
+// if (isOnlyOnePositive(a, b, c)) {
+//     console.log("A, B, C sonlaridan faqat bittasi musbat son.");
+// } else {
+//     console.log("A, B, C sonlaridan faqat bittasi musbat emas.");
+// }
